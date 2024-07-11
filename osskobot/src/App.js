78935ee22c8e627a3a28deb34bc040f37dd49af5
rktlskan021 <rktlskan021@gmail.com>
2024-07-11 18:35:21 +0900

    feat: App.js Routing

--- osskobot/src/App.js
+++ osskobot/src/App.js
@@ -1,8 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Join from './screens/Join';
+import Mypage from './screens/Mypage';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/join" element={<Join/>}/>
+        <Route path="/mypage" element={<Mypage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ osskobot/src/screens/Join.js
@@ -0,0 +1,7 @@
+function Join(){
+    return (
+        <h1>회원가입 페이지입니다</h1>
+    )
+}
+
+export default Join;
