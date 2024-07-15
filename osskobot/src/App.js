1996a7890715ee912052931b90f54d6a96ea0611
rktlskan021 <rktlskan021@gmail.com>
2024-07-15 11:33:55 +0900

    feat: 라우팅 구현

--- osskobot/src/App.js
+++ osskobot/src/App.js
@@ -16,6 +16,14 @@ function App() {
         <Route path="/join" element={<Join/>}/>
         <Route path="/mypage" element={<Mypage/>}/>
         <Route path="/bookclick" element={<Bookclick/>}/>
+        {/* 각각 스크린 구현 후 라우팅 시켜주면 됨
+          /bookclick/talk : 대화하기 스크린
+          /bookclick/bookquiz : 독서퀴즈 스크린
+          /bookclick/knowledgequiz : 지식 키우기 스크린
+        */}
+        {/* <Route path="/bookclick/talk" element={<Home/>}/>
+        <Route path="/bookclick/bookquiz" element={<Home/>}/>
+        <Route path="/bookclick/knowledgequiz" element={<Home/>}/> */}
       </Routes>
     </Router>
   );
