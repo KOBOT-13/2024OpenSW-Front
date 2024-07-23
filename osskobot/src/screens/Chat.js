1bfaaf914f1d694c462335e84cc950c17d584273
rktlskan021 <rktlskan021@gmail.com>
2024-07-23 16:11:28 +0900

    feat: #15 채팅 기능 완성
    date format을 위한 date-fns 패키지 설치
    채팅 기능 및 테스트 코드 작성

--- osskobot/src/screens/Chat.js
+++ osskobot/src/screens/Chat.js
@@ -1,16 +1,56 @@
 import image from '../assets/profile.png';
 import styles from './Chat.module.css';
-import { IoSend } from "react-icons/io5";
 import ChatMsg from '../components/ChatMsg/ChatMsg';
 
-const messages = [
-    { id: 1, message: 'Hello!', time: '10:00 AM', isOwnMessage: false },
-    { id: 2, message: 'Hi, Alice!', time: '10:01 AM', isOwnMessage: true },
-    { id: 3, message: 'How are you?', time: '10:02 AM', isOwnMessage: false },
-    { id: 4, message: 'I am good, thanks!', time: '10:03 AM', isOwnMessage: true },
-];
+import { format } from 'date-fns';
+import { IoSend } from "react-icons/io5";
+import { useEffect, useState, useRef } from 'react';
 
 function Chat() {
+    const messagesEndRef = useRef(null);
+    const [messages, setMessages] = useState([]); 
+    const id = messages.length + 1;  // 메시지의 id를 설정하는 방법 중 하나
+    const [msg, setMsg] = useState("");
+
+    const handleChatInput = (e) => {
+        setMsg(e.target.value);
+    }
+
+    const chatBotChatting = () => {
+        const newMsg = {
+            id: id+1,
+            message: "챗봇의 채팅입니다.",
+            time: format(new Date(), 'hh:mm aa'),
+            isOwnMessage: true
+        };
+
+        setMessages((prevMessages) => [...prevMessages, newMsg]);
+    }
+
+    const onClickChatBtn = () => {
+        if(msg === ""){
+            return;
+        }
+        const newMsg = {
+            id: id,
+            message: msg,
+            time: format(new Date(), 'hh:mm aa'),
+            isOwnMessage: false
+        };
+
+        setMessages((prevMessages) => [...prevMessages, newMsg]);
+
+        setMsg("");
+
+        chatBotChatting();
+    }
+
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [messages]);
+
     return (
         <div className={styles.mainContainer}>
             <div className={styles.imgChatDiv}>
@@ -24,11 +64,12 @@ function Chat() {
                             isOwnMessage={msg.isOwnMessage}
                         />
                     ))}
+                    <div ref={messagesEndRef} />
                 </div>
             </div>
             <div className={styles.inputDiv}>
-                <input className={styles.chatInput} type="text"></input>
-                <button className={styles.chatBtn}><IoSend size={20} /></button>
+                <input className={styles.chatInput} onChange={handleChatInput} value={msg} type="text"></input>
+                <button className={styles.chatBtn} onClick={onClickChatBtn} ><IoSend size={20} /></button>
             </div>
         </div>
     )
