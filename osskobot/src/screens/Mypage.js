fc588c87e8f75cf70eb6ee71e5b177c8f0981b23
rktlskan021 <rktlskan021@gmail.com>
2024-07-24 12:41:36 +0900

    feat: #21 나의 독후활동 구현
    각각 버튼 생성 이후 클릭 시 색상 추가
    버튼마다 다른 컴포넌트 생성

--- osskobot/src/screens/Mypage.js
+++ osskobot/src/screens/Mypage.js
@@ -1,13 +1,19 @@
-import {format} from 'date-fns';
-import {ko} from 'date-fns/locale'
+import { format } from 'date-fns';
+import { ko } from 'date-fns/locale'
+import { useState } from 'react';
 import styles from './Mypage.module.css';
 import image from '../assets/profile.png';
 
-function Mypage(){
+function Mypage() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const btns = ['내가 읽은 책' ,'이전 대화', '독후감', '퀴즈기록', '내가 쓴 글'];
+    const handleButtonClick = (index) => {
+        setActiveIndex(index);
+    };
+
     const nickname = "이재영";
-    const date = format(new Date("2002-12-04"), 'PPP', {locale: ko});
-    console.log(date);
-    return(
+    const date = format(new Date("2002-12-04"), 'PPP', { locale: ko });
+    return (
         <div className={styles.mainContainer}>
             <div className={styles.profileDiv}>
                 <img src={image} className={styles.profileImg} />
@@ -17,6 +23,28 @@ function Mypage(){
                 </div>
             </div>
             <div className={styles.myReadActDiv}>
+                <h3 style={{marginBottom:"0"}}>나의 독후활동</h3>
+                <div className={styles.btnsDiv}>
+                    {btns.map((label, index) => (
+                        <button
+                            key={index}
+                            className={`${styles['btn']} ${activeIndex === index ? styles['active'] : ''}`}
+                            onClick={() => handleButtonClick(index)}
+                        >
+                            {label}
+                        </button>
+                    ))}
+                </div>
+                <hr/>
+                <div className={styles.readActDiv}>
+                    {
+                        activeIndex === 0 ? <div>0</div> 
+                        : activeIndex === 1 ? <div>1</div>
+                        : activeIndex === 2 ? <div>2</div>
+                        : activeIndex === 3 ? <div>3</div>
+                        : <div>4</div>
+                    }
+                </div>
 
             </div>
         </div>
