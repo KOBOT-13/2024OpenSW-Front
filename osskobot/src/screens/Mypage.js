72a3519dbd5c17ec550234ec384636ac81212dd2
rktlskan021 <rktlskan021@gmail.com>
2024-07-24 11:05:59 +0900

    feat: #21 프로필 수정 레이아웃 구현
    프로필 사진, 닉네임, 생년월일 UI 구현

--- osskobot/src/screens/Mypage.js
+++ osskobot/src/screens/Mypage.js
@@ -1,6 +1,25 @@
+import {format} from 'date-fns';
+import {ko} from 'date-fns/locale'
+import styles from './Mypage.module.css';
+import image from '../assets/profile.png';
+
 function Mypage(){
+    const nickname = "이재영";
+    const date = format(new Date("2002-12-04"), 'PPP', {locale: ko});
+    console.log(date);
     return(
-        <h1>마이페이지 입니다</h1>
+        <div className={styles.mainContainer}>
+            <div className={styles.profileDiv}>
+                <img src={image} className={styles.profileImg} />
+                <div className={styles.userInfoDiv}>
+                    <p><u>{nickname}</u>님 안녕하세요.</p>
+                    <p>생년월일 : {date}</p>
+                </div>
+            </div>
+            <div className={styles.myReadActDiv}>
+
+            </div>
+        </div>
     )
 }
 
