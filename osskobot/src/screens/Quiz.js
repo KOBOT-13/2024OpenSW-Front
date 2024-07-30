6f885776ef603d2419ace7dc3ff71c2b5a29ea0f
OnlyWonHand <113083948+IamWonILuvWon@users.noreply.github.com>
2024-07-30 14:30:23 +0900

    feat: 결과 공유하기 했을 때 독후활동공유 페이지로 이동하도록

--- osskobot/src/screens/Quiz.js
+++ osskobot/src/screens/Quiz.js
@@ -1,10 +1,13 @@
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import QuizList from '../forms/QuizList';
 import Question from '../components/Quiz/Question';
 import './Quiz.css'; // 추가된 CSS 파일 import
 
 const Quiz = () => {
+  const navigate = useNavigate();
+
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [showResult, setShowResult] = useState(false);
@@ -43,7 +46,9 @@ const Quiz = () => {
     setShowAnswer(false); // 답변 표시 숨기기
     setIsCorrect(false); // 정답 여부 초기화
   };
-
+  const goCommunity = () =>{
+    navigate(`/bookclick/${bookId}`);
+  }
   return (
     <div className="quiz-container">
       {showResult ? (
@@ -53,7 +58,7 @@ const Quiz = () => {
           {score > 3 ? <p>훌륭해요! 독서를 열심히 했군요!</p> : <p>다시해볼까? 랄랄랄라라</p>}
           <div className="actions">
             <button className="retry-button" onClick={retryQuiz}>다시 시도하기</button>
-            <button className="share-button">결과 공유하기</button> 
+            <button className="share-button" onClick = {goCommunity}>결과 공유하기</button> 
             {/* 결과 공유하기 클릭시에 커뮤니티로 이동되도록  */}
           </div>
         </div>
